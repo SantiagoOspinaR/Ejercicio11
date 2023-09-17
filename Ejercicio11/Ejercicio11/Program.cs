23f2d1a4a650cfe2ca84dbd3ec05975158b21b14
SantiagoOspinaR <ospinasantiago41@gmail.com>
2023-09-17 03:00:54 -0500

    Ejercicio 11 completo

---
+++ Ejercicio11/Ejercicio11/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.Design;
+
+public class Ejercicio11
+{
+    static void Main(string[] args)
+    {
+        /* Construir un algoritmo que pida N números por pantalla,
+        cuando el usuario ingrese un número negativo, el programa debe finalizar y mostrar en consola
+        la cantidad total de números positivos que ingresó hasta antes de ingresarse el número negativo. */
+
+
+        int contador = 0;
+        int n, num;
+
+        Console.WriteLine("Ingrese la cantidad de números que desea evaluar:  ");
+        n = Convert.ToInt32(Console.ReadLine());
+
+        do
+        {
+            Console.WriteLine("Ingrese un número:  ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (num >= 0)
+            {
+                contador++;
+
+            }
+            else
+            {
+                break;
+            }
+        
+        } while (contador < n);
+        Console.WriteLine(String.Format("La cantidad de números positivos ingresados es de {0}", contador));
+
+
+
+
+    }
+
+
+
+
+}
